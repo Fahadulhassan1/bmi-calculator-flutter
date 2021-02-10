5b85edc665e25b8a549961c12b40d863f9f55a39
Fahadulhassan1 <fahad.khalid01234@gmail.com>
2021-02-10 23:28:43 +0500

    resuability

---
+++ lib/input_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: ReusableCard()),
+                  Expanded(child: ReusableCard()),
+                ],
+              ),
+            ),
+            Expanded(child: ReusableCard()),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard()),
+                Expanded(child: ReusableCard())
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({Key key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
